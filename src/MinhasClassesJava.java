5d9ddf2106de865ace9e5f91292053c110eca7c3
Wallekson <walisoncastor@gmail.com>
2023-03-08 06:26:41 -0300

    Exemplo pratico de Anatomia da Classes(variavel, metodo, identação)

---
+++ src/MinhasClassesJava.java
@@ -0,0 +1,21 @@
+public class MinhasClassesJava {
+    public class MinhaClasseJava {
+
+        public static void main (String [] args) {
+
+            String primeiroNome = "Wallekson";
+            String segundoNome = "Castor";
+
+            String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+            System.out.println(nomeCompleto);
+
+
+        }
+
+        public static String nomeCompleto (String primeiroNome, String segundoNome) {
+            return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+        }
+
+    }
+
+}
