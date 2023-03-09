039bdb5a275aec910797c4d68a95c57b0249354c
Perkles <walisoncastor@gmail.com>
2023-03-09 13:39:00 -0300

    Increments - Boolean and Numbers

---
+++ src/Operadores/Operadores/src/OperadoresParte2.java
@@ -0,0 +1,50 @@
+package Operadores.Operadores.src;
+
+public class OperadoresParte2 {
+
+    public static void main(String[] args) {
+        /* boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        System.out.println(variavel);
+
+
+        // Para inverter definitivamente
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+
+        /* int numero = 5;
+
+        // x Repetição
+
+        numero++;
+        //numero = numero + 1;
+
+        System.out.println(numero);
+
+
+        //Variação
+
+        System.out.println(numero ++);
+
+        System.out.println(numero);
+
+
+       //Incrementar antes de imprimir
+
+       System.out.println( ++ numero);
+
+       System.out.println(numero);
+
+
+       //Decrementar
+
+       System.out.println( -- numero);
+
+       System.out.println(numero);*/
+    }
+}
