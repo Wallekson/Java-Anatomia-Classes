e862cffdd342a710f087d0cf8b1a254fae167b22
Perkles <walisoncastor@gmail.com>
2023-03-09 14:03:03 -0300

    Ternário

---
+++ src/Operadores/Operadores/src/Ternario.java
@@ -0,0 +1,26 @@
+package Operadores.Operadores.src;
+
+public class Ternario {
+
+    public static void main(String[] args) {
+    /*  int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        //Sempre verificar se essa expressão é booleana
+        //Não se limita apenas a String
+        // Ex: int resultado = a == b ? "1" : "0";
+
+    /* forma comum da Condicional
+        String resultado = "";
+
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado); */
+
+    }
+}
