55f72dbebffaaad18cc01650feddf6b6d8c00a88
Wallekson <walisoncastor@gmail.com>
2023-03-08 21:56:24 -0300

    Variaveis e Constantes

--- src/TiposVariaveis/src/TiposVariaveis.java
+++ src/TiposVariaveis/src/TiposVariaveis.java
@@ -1,19 +1,26 @@
+package TiposVariaveis.src;
+
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        /* ESTUDAR TIPOS PRIMITIVOS
+        // ESTUDAR CLASSE STRING QUE REPRESENTA TEXTO NA APLICAÇÃO
     
-       /* byte idade = 123;
+        byte idade = 123;
           short ano = 2021;
           int cep = 21070333; // se começar com zero pode ser que tenha que mudar o tipo de variavel
           long cpf = 98765432109L; // tipo long o conteudo deve terminar com 'L'. Se começar com zero pode ser que tenha que mudar o tipo de variavel
           float pi = 3.14F; // // tipo float o conteudo deve terminar com 'F'
-          double salario = 1275.33;*/
-    
-           
+          double salario = 1275.33;
+
+        String meuNome = "WALLEKSON CASTOR";
+
+        final double VALOR_DE_PI = 3.14;
 
     short numeroCurto = 1;
     int numeroNormal = numeroCurto;
-    short numeroCurto2 = (short) numeroNormal;
+    int numeroCurto2 = numeroNormal;
 
     /*short numeroCurto = 1;
       int numeroNormal = numeroCurto;
