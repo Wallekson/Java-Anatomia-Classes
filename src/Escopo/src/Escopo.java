ec0af2729d3d13f12b5e4cf09561e6a265d9630b
Wallekson <walisoncastor@gmail.com>
2023-03-10 23:48:23 -0300

    Scope

---
+++ src/Escopo/src/Escopo.java
@@ -0,0 +1,37 @@
+public class Escopo {
+    public static void main(String[] args) {
+       /*
+        public class Conta {
+            //variavel da class conta
+            double saldo = 10.0;
+
+            public void scar (Double valor) {
+                // variavel local de método
+                double nomeSaldo = saldo - valor;
+            }
+
+            public void imprimirSaldo(){
+                // disponivel em toda classe
+                System.out.println(saldo);
+                // somente  o método scar conhece esta variavel
+                System.out.println(nomeSaldo);
+            }
+
+            public double calcularDividaExponencial(){
+                // variavel local de método
+                double valorParcela = 50.0;
+                double valorMontante = 0.0; // começando a variavel
+                for (int x=1, x<=5, x++) {
+                // x variavel sera reiniciada a cada execução
+                double valorCalculado = valorParcela * x;
+                valorMontante = valorMontante + valorCalculado;
+                }
+                //escopo de fluxo
+                // x e o valorCalculado nunca estarão disponiveis
+
+                return valorMontante;
+            }
+        }
+       */
+    }
+}
