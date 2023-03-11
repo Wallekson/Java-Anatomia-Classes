1b2a278128d63064352445fa14d3437a0b96711c
Wallekson <walisoncastor@gmail.com>
2023-03-10 21:54:08 -0300

    Method

---
+++ src/Metodos/Metodos/src/Metodo.java
@@ -0,0 +1,61 @@
+public class Metodo {
+    public static void main(String[] args) {
+/*
+        //Exemplos
+        somar(int n1, int n2){}
+
+        abrirConexao(){}
+
+        concluirProcessamento(){}
+
+        findById(int id){} // não se assuste, voce verá muitos métodos em ingles
+
+        calcularImprimir(){} // ha algo errado com esse método, ele deveria ter apenas uma responsbilidade
+
+        public double somar (int num1, int num2){
+            // Lógica - Finalidade do método
+            return...;
+        }
+
+
+        public void imprimir(String texto) {
+            // Lógica - Finalidade do método
+            //Aqui nâo precisa de return
+            // Pois não será retornado nenhum resultado
+        }
+
+
+        //throws Exception: indica que o método
+        // ao ser utilizado poderá gerar uma exceção
+        public double dividir(int dividendo, int divisor) throws Exception{}
+
+
+        // este método não pode ser visto por outras classes
+        // no próprio projeto
+        private void metodoPrivado(){}
+
+
+        //alguns equivocos estruturais
+        public void validar () {
+            // este método deveria retornar algum valor
+            // no caso boolean (true ou false)
+        }
+
+        public void cacularEnviar {
+            // um método deve representar uma única responsabilidade
+        }
+
+        public void gravarCliente(String cpf, String nome) {
+            // este método tema a finalidade de gravar
+            // informações de um cliente, por que não cria um
+            // objeto cliente e passar como  parâmetro
+            // Veja abaixo:
+        }
+
+        public void gravarCliente (Cliente cliente){}
+        //ou
+        public void gravar (Cliente cliente){}
+        
+ */
+    }
+}
