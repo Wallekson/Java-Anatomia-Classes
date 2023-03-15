4105d51f3e2ef89f7f9dc63e003101a00c72d0d1
Wallekson <walisoncastor@gmail.com>
2023-03-15 06:38:41 -0300

    Exercise Flux Control

--- src/LogicaCondicional-E-Controle-de-Fluxo/src/ExercicioOperadoresLogicos.java
+++ src/LogicaCondicional-E-Controle-de-Fluxo/src/ExercicioOperadoresLogicos.java
@@ -10,14 +10,14 @@ public class ExercicioOperadoresLogicos {
         boolean b3 = true;
         boolean b4 = false;
 
-        System.out.println("b1 && b2" + (b1 && b2));
-        System.out.println("b1 && b3" + (b1 && b3));
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
 
-        System.out.println("b2 || b3" + (b2 || b3));
-        System.out.println("b2 || b4" + (b2 || b4));
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
 
-        System.out.println("b1 ^ b3" + (b1 ^ b3));
-        System.out.println("b4 ^ b1" + (b4 ^ b1));
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
+        System.out.println("b4 ^ b1 " + (b4 ^ b1));
 
         System.out.println(!b1);
         System.out.println(!b2);
@@ -26,8 +26,9 @@ public class ExercicioOperadoresLogicos {
         int i2 = 5;
         float f1 = 20f;
         float f2 = 50f;
-        System.out.println("((i1 * i2) < (f1 - f2) )) " + (((i1 * i2) < (f1 - f2)) && true));
+        System.out.println("((i1 + i2) < (f1 - f2) )) " + (((i1 + i2) < (f1 - f2)) && true));
         System.out.println("((i1 > i2) || (f2 < f1) )) " + ((i1 > i2) || (f2 < f1)));
+
 
         double salarioMensal = 11893.58d;
         double mediaSalario = 10500d;
@@ -35,7 +36,7 @@ public class ExercicioOperadoresLogicos {
         int quantidadeDependentes = 4;
         int mediaDependentes = 2;
 
-        System.out.println((salarioMensal < mediaSalario) &&(quantidadeDependentes >= mediaDependentes));
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
 
         boolean salarioBaixo = salarioMensal < mediaSalario;
         boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
